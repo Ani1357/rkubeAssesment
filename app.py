688bf02d8f5efbd4144812fbf4b60d16cae716eb
Ani Argjiri <ani.argjiri@gmail.com>
2023-01-16 10:54:01 +0100

    Cleaned up the app code and configured the Swagger UI

--- app.py
+++ app.py
@@ -1,40 +1,26 @@
-from apispec import APISpec
-from apispec.ext.marshmallow import MarshmallowPlugin
-from apispec_webframeworks.flask  import FlaskPlugin
-from flask import Flask, jsonify
+from flask import Flask
 from azure.identity import EnvironmentCredential 
 from azure.storage.blob import BlobServiceClient
 import json, os, time
-
+from flasgger import Swagger 
 
 app = Flask(__name__)
 
+# Reading the configuration
 with open("/etc/app.conf", "r") as config:
     data = json.load(config)
     container_name = data["storage_config"]["container_name"]
     storage_account_url = data["storage_config"]["storage_account_url"]
 
+# Swagger init
+template = {"info": {"description": "API <style>.models {display: none !important}</style>"}}
+swagger = Swagger(app, template=template)
 
-@app.route('/')
-def nothing_here():
-    return "Nothing to see here. But it shows it works"
-
-
-spec = APISpec(
-    title='A simple app',
-    version='3.0.1',
-    openapi_version='3.0.0',
-    plugins=[FlaskPlugin(), MarshmallowPlugin()]
-)
-
-@app.route('/api/swager.json')
-def swager_spec():
-    return jsonify(spec.to_dict())
-
-
+# Logging in via a service principal
 az_creds = EnvironmentCredential()
 client   = BlobServiceClient(account_url=storage_account_url, credential=az_creds)
 
+# Function to read the names of the files inside the container
 def get_blob_list():
     list_of_blobs = []
     blobs = client.get_container_client(container_name).list_blobs()
@@ -42,31 +28,33 @@ def get_blob_list():
         list_of_blobs.append(b.name)
     return list_of_blobs
 
+# Root path of the app
+@app.route('/')
+def nothing_here():
+    return "Nothing to see here. But it shows it works"
 
+# API endpoint
 @app.route('/files')
 def get_files():
-    """Get list of objects
-        ---
-        get:
-            description: Fetch list of items in from object storage
-            response:
-                200:
-                    description: A list of items from the object bucket
-                    content:
-                        application/json:
-                            schema:
-                            type: array
-                            items:
-                            properties:
-                              fileName:
-                              type: string
-                500:
-                    description: Unable to fetch file list from container
 
-    """
+    """Endpoint returning a list of files stored in an Azure container.
+    Returns a json array of strings.
+    ---
+    definitions:
+      Files:
+        type: array
+        properties:
+          file_name:
+            type: string
+    responses:
+      200:
+        description: A list of files stored in an Azure container 
+      500:
+        description: Unable to fetch file list from container
+"""
 
     return get_blob_list()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
